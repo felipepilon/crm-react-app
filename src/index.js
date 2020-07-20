ee5bcbfe8377bd2841a923ecd87ceb9e48e154d3
Felipe Pilon <pilonfelipe@gmail.com>
2020-07-20 19:00:40 -0300

    Publishing to Heroku.

--- src/index.js
+++ src/index.js
@@ -54,6 +54,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log(`[ENV=${(process.env.NODE_ENV || "NOT DEFINED")}]`);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
